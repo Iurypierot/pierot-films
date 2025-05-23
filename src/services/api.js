ab662cbb4d409898175afccf648e7d3ed99e93c2
iurypierot <iurypierot@hotmail.com>
2025-05-23 17:59:59 -0300

    feat: implementa funções para consumo da API TMDb com tratamento de erros e busca de filmes, séries, pessoas, créditos e vídeos

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
-
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     params: {
-        api_key: '97a670b3004c7d520eb0a1af5d460913',
+        api_key: process.env.REACT_APP_TMDB_API_KEY,
         language: 'pt-BR',
         page: 1
     }
